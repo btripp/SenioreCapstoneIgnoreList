d9d0a52c723cc88569220abb63f5c9c1a545dd16
Brandon <btrippcsci@gmail.com>
2012-03-06 23:34:25 -0500

    THis is the initial seniorcapstone with the ignoreList

---
+++ Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace AugustaStateUniversity.SeniorCapstoneIgnoreList
+{
+    static class GuidList
+    {
+        public const string guidSeniorCapstoneIgnoreListPkgString = "4b3d1ac0-b4ef-4ae4-bcd2-7eb2c7a82c94";
+        public const string guidSeniorCapstoneIgnoreListCmdSetString = "89b2f6e4-8338-484c-a64a-1ef024a4621c";
+        public const string guidToolWindowPersistanceString = "7cffc514-66b4-442a-8fef-ec38f11a1c75";
+
+        public static readonly Guid guidSeniorCapstoneIgnoreListCmdSet = new Guid(guidSeniorCapstoneIgnoreListCmdSetString);
+    };
+}
---
+++ MyControl.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AugustaStateUniversity.SeniorCapstoneIgnoreList
+{
+    /// <summary>
+    /// Interaction logic for MyControl.xaml
+    /// </summary>
+    public partial class MyControl : UserControl
+    {
+        public MyControl()
+        {
+            InitializeComponent();
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions")]
+
+        private void ignoreListAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ignoreListTextBox.Text != "")
+            {
+                string file;
+                ignoreList.Items.Add(ignoreListTextBox.Text);
+                file = ignoreListTextBox.Text;
+                ignoreListTextBox.Clear();
+                MessageBox.Show("\"" + file + "\"" + " was added to the ignore list", "File Added");
+            }
+            else
+            {
+                MessageBox.Show("You must enter something in the textbox.", "Nothing Added");
+            }
+        
+        }
+    }
+}
